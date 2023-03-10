11465f588c17856d2f2b788254279fb57077ed04
Joshua <120212106+jminor90@users.noreply.github.com>
2023-03-10 14:01:12 -0700

    Updating overall file structure

---
+++ client/src/pages/Genre.js
@@ -0,0 +1,10 @@
+function Genre() {
+
+    return (
+        <div>
+            <h1>Genre</h1>
+        </div>
+    );
+}
+
+export default Genre;
---
+++ client/src/pages/OffTopic.js
@@ -0,0 +1,10 @@
+function OffTopic() {
+
+    return (
+        <div>
+            <h1>OffTopic</h1>
+        </div>
+    );
+}
+
+export default OffTopic;
