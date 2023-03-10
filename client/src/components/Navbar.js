32e3a041612c1c90559f59772cc6c09420290fd3
adam hovda <adamhovda@gmail.com>
2023-03-10 14:31:37 -0700

    added nav, bootstrap, json scripts

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,26 @@
-function Navbar() {
 
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    );
-}
+
+
+        function Navbar() {
+
+            return (
+                <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                    <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"></img>
+                    Bootstrap
+                    </a>
+                        <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+        
+                        <h2>Sign In</h2>
+                </div>
+        
+                </nav>
+            );
+        }
+
 
 export default Navbar;
