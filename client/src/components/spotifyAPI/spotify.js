9f15259a87b3ed69c100440dc4ea75766f969d22
Etowww <etow.engineering@gmail.com>
2023-03-14 11:05:53 -0700

    Added logic that allows users to click on a specific album to get the details of the album and allow them to add comments and publish their thoughts to a thread

--- client/src/components/spotifyAPI/spotify.js
+++ client/src/components/spotifyAPI/spotify.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, InputGroup, FormControl, Button, Row, Card, Col } from 'react-bootstrap';
@@ -8,7 +7,7 @@ import { useState, useEffect } from 'react';
 const CLIENT_ID = "66511bd43321461caae45c7644ef2572";
 const CLIENT_SECRET = "561bf37ea1bb4312825fe6a6d1c81689";
 
-function App() {
+function SpotifyApi() {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
   const [albums, setAlbums] = useState([]);
@@ -56,3 +55,104 @@ function App() {
       });
   }
   console.log(albums)
+
+// Function that handles a selected album and its publishing functionality
+function AlbumDetails({ album }) {
+    const [commentInput, setCommentInput] = useState("");
+    const [genre, setGenre] = useState("");
+
+    const handleCommentInputChange = (event) => {
+      setCommentInput(event.target.value);
+    }
+
+    const handlePublishButtonClick = () => {
+      const postInfo = {
+        albumImage: album.images[0].url,
+        albumName: album.name,
+        albumArtist: `${searchInput}`,
+        comment: commentInput,
+        genre: genre
+      };
+      console.log(postInfo);
+      setCommentInput("");
+      // Need to add a new function to redirect the user to the page where there post is published
+    }
+
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={album.images[0].url} />
+              <Card.Body>
+                <Card.Title>{album.name}</Card.Title>
+                <Card.Text>
+                  By: {searchInput}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+            <input type="text" placeholder="Add a comment" value={commentInput} onChange={handleCommentInputChange} />
+            <div>
+              <select value={genre} onChange={(event) => setGenre(event.target.value)}>
+                <option value="">Select a thread location for your post</option>
+                <option value="Rock">Rock</option>
+                <option value="Hip Hop">Hip Hop</option>
+                <option value="Electronic">Electronic</option>
+                <option value="Country">Country</option>
+                <option value="Classical">Classical</option>
+              </select>
+            </div>
+            <button onClick={handlePublishButtonClick}>Publish</button>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+
+// What is Initially shown on the page
+// Conditional logic for when an album is selected
+  return (
+    <div className="App">
+      {selectedAlbum ? (
+        <AlbumDetails album={selectedAlbum} />
+      ) : (
+        <div>
+          <Container>
+            <InputGroup className="mb-3" size="lg">
+              <FormControl
+                placeholder="Search For Artist"
+                type="input"
+                onKeyPress={event => {
+                  if (event.key == "Enter") {
+                    search();
+                  }
+                }}
+                onChange={event => setSearchInput(event.target.value)}
+                />
+              <Button onClick={search}>
+                Search
+              </Button>
+            </InputGroup>
+          </Container>
+          <Container>
+            <Row className="mx-2 row row-cols-4">
+              {albums.map( (album, i) => {
+                console.log(album);
+                return(
+                  <Card key={i} onClick={() => setSelectedAlbum(album)}>
+                    <Card.Img src={album.images[0].url} />
+                    <Card.Body>
+                      <Card.Title>{album.name}</Card.Title>
+                    </Card.Body>
+                  </Card>
+                )
+              })}
+            </Row>
+          </Container>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SpotifyApi;
